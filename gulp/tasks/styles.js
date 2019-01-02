5519dee6e82fefa8cb4c302f0ebbaac136787145
BBrownley <brendobrownley@yahoo.com>
2019-01-02 15:37:51 -0500

    Comitted additional files

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,14 +1,34 @@
-const
-
-gulp = require("gulp"),
-postcss = require("gulp-postcss"),
-autoprefixer = require("autoprefixer"),
-cssVars = require("postcss-simple-vars"),
-nested = require("postcss-nested"),
-cssImport = require("postcss-import");
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+const mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
-  return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([autoprefixer, cssVars, nested, cssImport]))
-  .pipe(gulp.dest("./app/temp/styles"));
+
+  //enter gulp css to use this task
+
+  //Be careful how these lines are spaced out - It could cause errors
+
+  //cssImport should be the first task
+
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
+
+  //We must throw an error before the function ends
+
+  .on("error", function(errorInfo) {
+    console.log(errorInfo.toString());
+    this.emit("end"); //The watch task wants to know when these tasks end, hence the .emit("end")
+                      //It will just see that the task ended, like it normally would
+  })
+
+  .pipe(gulp.dest('./app/temp/styles'));
+
+  /*
+    -We use return because gulp.src is an asynchronous function
+  */
+
 });
